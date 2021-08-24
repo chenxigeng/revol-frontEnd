b97702b3aab04c999636e7c012a937ecb5a1b906
WIN-LF1G59O74K7\Administrator <869385487@qq.com>
2021-08-24 23:39:26 +0800

    cxg

--- src/pages/demo/demo.js
+++ src/pages/demo/demo.js
@@ -1,2 +1,9 @@
 import '../../assets/scripts/common/common.js'
 import './demo.scss'
+import $ from 'jquery';
+
+let el_btnToggle = $('.btn-toggle')
+
+el_btnToggle.click(() => {
+  console.log('dfgggg')
+})
